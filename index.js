1b54c32f74a17fc00e55ffb1301254d5a125fd0f
SangHoon <joa3668@gmail.com>
2022-02-22 02:13:58 +0900

    Add: 모달, iframe 메인에 추가

---
+++ index.js
@@ -0,0 +1,33 @@
+// modal
+var modal = document.querySelector('.modal');
+var closeBtn = document.querySelector('.closeBtn');
+var todayCloseBtn = document.querySelector('.todayCloseBtn');
+
+function closeModal() {
+  modal.style.display = 'none';
+  document.body.classList.remove('stopScroll');
+}
+
+function setCookie(name, value, expiredays) {
+  var todayDate = new Date();
+  todayDate.setDate(todayDate.getDate() + expiredays);
+  document.cookie =
+    name + '=' + value + '; path=/; expires=' + todayDate.toUTCString() + ';';
+}
+
+function closeTodayModal() {
+  setCookie('popUpToday', 'close', 1);
+  closeModal();
+}
+
+closeBtn.addEventListener('click', closeModal);
+todayCloseBtn.addEventListener('click', closeTodayModal);
+
+window.onload = function () {
+  if (document.cookie != 'popUpToday=close') {
+    modal.style.display = 'block';
+    document.body.classList.add('stopScroll');
+  } else {
+    return;
+  }
+};
