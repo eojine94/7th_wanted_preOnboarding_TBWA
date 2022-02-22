9a5736503f95cbde561c7bd46bcd5aee8f477520
unknown <tohjbin2@gmail.com>
2022-02-22 21:18:44 +0900

    Add: NavAndFooter작업물 index로 이동

--- index.js
+++ index.js
@@ -31,3 +31,66 @@ window.onload = function () {
     return;
   }
 };
+
+// MEMO: nav - 해당 위치로 스크롤 이동
+var selectMoveToVideo = document.querySelector('#toVideo');
+var selectMoveToFlowerClass = document.querySelector('#toFlowerClass');
+
+var toVideo = document.querySelector('.ytContainer');
+var toFlowerClass = document.querySelector('.home_contents_class');
+
+selectMoveToVideo.addEventListener('click', moveToVideo);
+selectMoveToFlowerClass.addEventListener('click', moveToFlowerClass);
+
+function moveToVideo() {
+  toVideo.scrollIntoView({ behavior: 'smooth' });
+}
+function moveToFlowerClass() {
+  toFlowerClass.scrollIntoView({ behavior: 'smooth' });
+}
+
+// MEMO: nav - sticky 효과
+var nav = document.querySelector('.navSticky');
+var navBackground = document.querySelector('.stickyBackground');
+var rectTop = nav.getBoundingClientRect().top;
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > rectTop) {
+    nav.style.position = 'fixed';
+    nav.style.top = 0;
+    nav.style.height = '90px';
+    nav.style.borderBottom = 'solid 2px #ffcd32';
+    navBackground.style.position = 'fixed';
+    navBackground.style.width = '100%';
+    navBackground.style.height = '90px';
+    navBackground.style.backgroundColor = 'white';
+    navBackground.style.borderBottom = 'solid 2px #ffcd32';
+    navBackground.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+  } else {
+    nav.style.position = 'fixed';
+    nav.style.top = '80px';
+    nav.style.border = 'none';
+    navBackground.style = 'none';
+  }
+});
+
+// MEMO: footer - 새창으로 열기
+var facebookUrl = document.querySelector('.facebookUrl');
+var instaUrl = document.querySelector('.instaUrl');
+var youtubeUrl = document.querySelector('.youtubeUrl');
+
+facebookUrl.addEventListener('click', moveToFacebook);
+instaUrl.addEventListener('click', moveToInsta);
+youtubeUrl.addEventListener('click', moveToYoutube);
+
+function moveToFacebook() {
+  newPage = window.open('https://ko-kr.facebook.com/kukka.kr/');
+}
+function moveToInsta() {
+  newPage = window.open('https://www.instagram.com/kukkakorea/');
+}
+function moveToYoutube() {
+  newPage = window.open(
+    'https://www.youtube.com/channel/UC_zQakXCUPvjcfsU067zyCQ?view_as=subscriber'
+  );
+}
